815ad07b77324eea7d4376b35748698351644e1a
Wesely <wesely.ong@gmail.com>
2016-01-28 15:59:32 +0800

    update staggered GridView

--- weslib/src/main/java/tw/wesely/weslib/ui/cardview_recycle_list/CardViewImageAndTitleAdapter.java
+++ weslib/src/main/java/tw/wesely/weslib/ui/cardview_recycle_list/CardViewImageAndTitleAdapter.java
@@ -21,55 +21,56 @@ public class CardViewImageAndTitleAdapter extends RecyclerView.Adapter<CardViewI
 	private ViewGroup parent;
 	private boolean animate = true;
 
-	// Provide a reference to the views for each data item
-	// Complex data items may need more than one view per item, and
-	// you provide access to all the views for a data item in a view holder
-	public static class ViewHolder extends RecyclerView.ViewHolder {
-		// each data item is just a string in this case
-		public CardView mCardView;
-
-		public ViewHolder(CardView v) {
-			super(v);
-			mCardView = v;
-		}
-	}
-
 	// Provide a suitable constructor (depends on the kind of dataset)
-	public CardViewImageAndTitleAdapter(String[] myDataset) {
-		mDataset = myDataset;
-	}
+    public CardViewImageAndTitleAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
 
-	// Create new views (invoked by the layout manager)
-	@Override
-	public CardViewImageAndTitleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-												   int viewType) {
-		this.ctx = parent.getContext();
-		this.parent = parent;
+    // Create new views (invoked by the layout manager)
+    @Override
+    public CardViewImageAndTitleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                      int viewType) {
+        this.ctx = parent.getContext();
+        this.parent = parent;
 
-		CardView v = (CardView) LayoutInflater.from(parent.getContext())
-				.inflate(R.layout.cardview, parent, false);
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cardview, parent, false);
 //		v.setPreventCornerOverlap(false); // true: child view will overlap ON cards
 //		v.setRadius(0);
 //		v.setCardBackgroundColor(0xeeffee);
 //		v.setCardElevation(20f);
 //		v.setPadding(5,2,5,200);
-		ViewHolder vh = new ViewHolder(v);
-		return vh;
-	}
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
 
 	@Override
-	public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
-		View v = LayoutInflater.from(parent.getContext())
-				.inflate(R.layout.item_title_below_image, parent, false);
-		TextView tv = (TextView) v.findViewById(R.id.tvCardViewTitle);
-		tv.setText(mDataset[position]);
-		holder.mCardView.addView(v);
-	}
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_title_below_image, parent, false);
+        TextView tv = (TextView) v.findViewById(R.id.tvCardViewTitle);
+        tv.setText(mDataset[position]);
+        holder.mCardView.removeAllViews();
+        holder.mCardView.addView(v);
+    }
 
-	// Return the size of your dataset (invoked by the layout manager)
-	@Override
-	public int getItemCount() {
-		return mDataset.length;
-	}
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public CardView mCardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            mCardView = v;
+        }
+    }
 }
--- weslib/src/main/java/tw/wesely/weslib/ui/cardview_recycle_list/RecycleCardViewStaggeredGridActivity.java
+++ weslib/src/main/java/tw/wesely/weslib/ui/cardview_recycle_list/RecycleCardViewStaggeredGridActivity.java
@@ -21,7 +21,7 @@ public class RecycleCardViewStaggeredGridActivity extends AppCompatActivity {
 
 		// use this setting to improve performance if you know that changes
 		// in content do not change the layout size of the RecyclerView
-		mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize(false);
 
 		// use a linear layout manager
 //		mLayoutManager = new LinearLayoutManager(this);
